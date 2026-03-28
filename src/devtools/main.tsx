e487fc8ae619c8273665bbac1bd84223ed2046bf
Leonard Siu <831085+lsiu@users.noreply.github.com>
2026-03-28 23:40:33 +0800

    Fix panel. Now panel loads. But there is no ad data showing.

---
+++ src/devtools/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Panel from './Panel';
+import './index.css';
+
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <Panel />
+    </React.StrictMode>
+  );
+}
